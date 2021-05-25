f9f5b6496188f15e53f3c751de8d375c66d3a9b8
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-25 18:13:49 +0200

    Create getAverage (2of2)

--- makeHistogram.py
+++ makeHistogram.py
@@ -40,6 +40,27 @@ def makeHistogram(values, numBins, xLabel, yLabel, title=None):
     pylab.show()
 
 
+def count_longest_run(trial):
+    result = 1
+    max_result = 0
+    last_seen = trial[0]
+
+    for v in trial[1:]:
+        if v == last_seen:
+            result += 1
+        else:
+            if result > max_result:
+                max_result = result
+            last_seen = v
+            result = 1
+
+    # just in case the longest sequence would be at the end of your list...
+    if result > max_result:
+        max_result = result
+    return max_result
+
+
+
 # Implement this -- Coding Part 2 of 2
 def getAverage(die, numRolls, numTrials):
     """
@@ -52,8 +73,27 @@ def getAverage(die, numRolls, numTrials):
       - Choose appropriate labels for the x and y axes.
       - Returns the mean calculated
     """
-    # TODO
+    trial_results = []
+    for trials in range(numTrials):
+        rolls_results = []
+        for rolls in range(numRolls):
+            rolls_results.append(die.roll())
+        trial_results.append(rolls_results)
+    rolls_results = []
+
+    longest_runs = []
+    for trial in trial_results:
+        longest_runs.append(count_longest_run(trial))
+
+    longest_run_sum = sum(longest_runs)
+    longest_run_mean = longest_run_sum/numTrials
+
+    makeHistogram(longest_runs, 10, 'roll', 'value', title=None)
+
+    return longest_run_mean
 
 
 # One test case
+
+# print(getAverage(Die([1, 2, 3, 4, 5, 6, 6, 6, 7]), 5, 10))
 print(getAverage(Die([1, 2, 3, 4, 5, 6, 6, 6, 7]), 500, 10000))
